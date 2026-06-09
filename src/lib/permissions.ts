3094b6dd08376dd49191a4ccd1159418330e5cf0
Greyden <greydenscott@gmail.com>
2026-06-09 19:41:28 +1000

    Update package-lock.json and permissions.ts
    - Modified package-lock.json to add "peer": true for several dependencies and removed "libc" entries for various packages.
    - Updated BOT_PERMISSIONS constant in permissions.ts to reflect a new permissions value.

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,4 +1,4 @@
 // View Channels + Send Messages + Mention Roles + Connect
 // + Create Public Threads + Send Messages in Threads
 // + Manage Roles + Manage Nicknames + Manage Channels
-export const BOT_PERMISSIONS = 309821648272;
+export const BOT_PERMISSIONS = 309641481232;
